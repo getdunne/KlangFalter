645d1e5f39f3fcb595cfa98bb22c2153f15a6a65
Shane Dunne <shane@getdunne.com>
2020-01-03 13:14:21 -0500

    Bump version to 1.1.0 (Unify-specific)

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -49,7 +49,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "KlangFalter";
     const char* const  companyName    = "HiFi-LoFi";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "1.1.0";
+    const int          versionNumber  = 0x10100;
 }
 #endif
