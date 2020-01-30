562864ab2338ccc6150f9585ee2ec1d585259338
Shane Dunne <shane@getdunne.com>
2020-01-30 14:43:25 -0500

    v1.1.1: Update for macOS 10.9 compatibility

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -49,7 +49,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "KlangFalter";
     const char* const  companyName    = "HiFi-LoFi";
-    const char* const  versionString  = "1.1.0";
-    const int          versionNumber  = 0x10100;
+    const char* const  versionString  = "1.1.1";
+    const int          versionNumber  = 0x10101;
 }
 #endif
